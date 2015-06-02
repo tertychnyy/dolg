4cf13da3df90e1054493e1de5f448d424f1011b4
tertychnyy <itertychnyy@gmail.com>
2015-06-02 23:19:45 +0300

    /api/events initial

--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
 from app import app
+import json
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/api/events')
+def api_events():
+    data = {}
+    event1 = {'name': 'Поездка в Португалию',
+              'id': 1,
+              'startdate': '2015-08-04 05:35'}
+    event2 = {'name': 'Может в кальянную сходим?',
+              'id': 2,
+              'startdate': '2015-06-12 20:00'}
+    data['data'] = [event1, event2]
+    return json.dumps(data)
